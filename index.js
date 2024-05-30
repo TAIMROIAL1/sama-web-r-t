2da87cb060c004c0e233644bf29a98310ed1ef55
TAIMROIAL1 <taimroyalkh@gmail.com>
2024-05-30 19:39:29 +0300

    javascript only

---
+++ index.js
@@ -0,0 +1,10 @@
+const slides = [...document.querySelectorAll('.center')];
+const activeIndex = 0;
+
+console.log(slides);
+
+slides.forEach((slide, i) => {
+  slide.style.transform = `translateX(${i * 110}%)`;
+})
+
+'n';
