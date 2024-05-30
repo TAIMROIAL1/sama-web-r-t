4d61b27bb7fb352a0c026c682c89c6ed8fe52476
TAIMROIAL1 <taimroyalkh@gmail.com>
2024-05-30 23:54:40 +0300

    edited slider

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const slides = [...document.querySelectorAll('.center')];
+const containerSlides = document.querySelector('.cont');
+
 const activeIndex = 0;
 
 console.log(slides);
 
-slides.forEach((slide, i) => {
-  slide.style.transform = `translateX(${i * 110}%)`;
+const renderSlides = function(index = 0) {
+  const counter = -index;
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(${counter * 110}%)`;
+    counter++;
+  })
+}
+
+
+containerSlides.addEventListener('click', function(e) {
+  const clicked = e.closest('.btn');
+
+  if(!clicked) return;
+
+  if(clicked.classList.contains('button1'))
+      activeIndex = activeIndex === 0? slides.length - 1 : activeIndex--;
+    
+
+  if(clicked.classList.contains('button2'))
+      activeIndex = activeIndex === slides.length - 1? 0 : activeIndex++;    
+  
+    renderSlides(activeIndex)
 })
